604a0f8c4f08c6f3c405741984c8251cffeef8f0
Narges <narges.yousofzada1@gmail.com>
2026-06-11 14:14:18 +0430

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect, useContext } from "react";
-import { ThemeContext } from './context/ThemeContext';
-import Home from './pages/Home'
-import ProjectDetails from "./pages/ProjectDetails"
-import About from './pages/About'
-import StudentsDetails from './pages/StudentsDetails'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import SocialDetails from './pages/SocialDetails'
-import NotFound from './pages/NotFound'
-import Navbar from './components/Navbar'
+import React from "react"
 
 function App() {
-  const { theme } = useContext(ThemeContext);
-  return (
-    <div className={ theme === 'dark' ? 'dark' : 'light'}>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/projects/:id" element={<ProjectDetails />} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/Students/:id" element={<StudentsDetails />} />
-      <Route path="/projects" element={<Projects/>} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/social/:id" element={<SocialDetails />} />
-      <Route path="*" element={<NotFound/>} />
-    </Routes>
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center h-screen bg-blue-100">
+            <h1 className="text-4xl font-bold text-red-600 animate-bounce">
+                This is a new discovery!!!
+            </h1>
+        </div>
+    )
 }
-
-export default App;
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1 className="text-3xl text-green-500">Tailwind Works</h1>
             <Link to="/projects/1">Project 1</Link>
             <br />
             <Link to ="/projects/2">Project 2</Link>
